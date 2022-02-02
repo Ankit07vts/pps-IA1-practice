7b31d127bbbad2b84df5f6c2eec693014db459ae
Ankitvats <95743251+Ankit07vts@users.noreply.github.com>
2022-02-02 08:53:18 +0000

    ..

--- p1original.c
+++ p1original.c
@@ -1,2 +1,34 @@
+#include <stdio.h>
+
+ int input()
+{
+  int n;
+  printf("enter the numbers\n");
+  scanf("%d",&n);
+  return n;
 
 }
+
+int add(int a, int b)
+{
+  int sum;
+  sum=a+b;
+  return sum;
+}
+void output(int a,int b, int sum)
+{
+  printf("Sum of %d +%d is %d\n",a,b,sum);
+
+}
+
+int main()
+{
+  int a,b,sum;
+  a=input();
+  b=input();
+  sum=add(a,b);
+  output(a,b,sum)
+  return 0;
+}
+
+
