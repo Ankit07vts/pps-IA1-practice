9dfd6ad0b5f7dd76263e93bf5f8fe305923054eb
Ankitvats <95743251+Ankit07vts@users.noreply.github.com>
2022-02-02 09:28:39 +0000

    ..

---
+++ p3original.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+ int input_n()
+ {
+   int n;
+   printf("enter the Value of n\n");
+   scanf("%d",&n);
+   return n;
+ }
+
+ int sum_n(int n)
+ {
+   int i , sum=0;
+   for (i=0;i<=n;i++)
+   {
+   sum = i+Sum;
+   }
+   return sum;
+
+ }
+ void output(int n, int sum);
+ {
+   printf("the sum %d nos is %d",n,sum);
+
+ }
+
+ int main()
+ {
+    int x,sum;   
+    x=input_n();  
+    sum =sum_n();
+    output(n,sum);
+    return 0;
+
+   
+}  
+ 
